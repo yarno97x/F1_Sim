10285433fc72537d4e259f377f9d72fc7a439fd8
Yarno Grenier <127676205+yarno97x@users.noreply.github.com>
2025-07-03 13:22:13 -0400

    Basic car tests

--- car.py
+++ car.py
@@ -1,19 +1,33 @@
-from PU import PU
+from PU import *
 from tire import *
 from fuel import FuelTank
 
 class Car :
     def __init__(self, power_unit, tires, fuel_tank):
-        if any([not isinstance(power_unit, PU), not isinstance(tires, list), not isinstance(fuel_tank, FuelTank)]) :
-            raise TypeError("Car inputs not well formatted")
+        if not isinstance(power_unit, PU) :
+            raise TypeError("Not a power unit")
+        if not isinstance(tires, list) or len(tires) != 4 :
+            raise TypeError("Not a list of 4 tires")
+        if not isinstance(fuel_tank, FuelTank) :
+            raise TypeError("Not a a fuel tank")
         elif any([not isinstance(tire, Tire) for tire in tires]) :
             raise TypeError("tires should be a list of <Tire>")
         
-        self.PU = PU
+        self.power_unit = power_unit
         self.tires = tires
         self.fuel_tank = fuel_tank
         self.DRS = False
+    
+    def status(self):
+        lines = []
+        lines.append(f"{self.tires[0]}{' ' * 11}{self.tires[1]}")
+        lines.append(f"{8 * ' '}{self.power_unit}{8 * ' '}")
+        lines.append(f"{5 * ' '}{self.fuel_tank}{5 * ' '}")
+        lines.append(f"{self.tires[2]}{' ' * 11}{self.tires[3]}")
+        lines.append(f"{5 * ' '}DRS {'Enabled' if self.DRS else 'Disabled'}")
 
-    def get_tire_temperatures(self) :
-        return [tire.temperature for tire in self.tires]
+        centered = "\n".join(lines)
+        return centered
+
+
     
--- tests/test_car.py
+++ tests/test_car.py
@@ -1,4 +1,57 @@
 import pytest
 import sys
-import os
+import os, random
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from PU import PU, PU_constructors
+from tire import *
+from fuel import FuelTank
+from car import *
+
+@pytest.fixture
+def power_unit() :
+    return PU(random.choice(list(PU_constructors)))
+
+@pytest.fixture
+def tires() :
+    tire_kind = random.choice([Soft, Medium, Hard, Inter, Wet])
+    return [tire_kind() for _ in range(4)]
+
+@pytest.fixture
+def fuel_tank() :
+    return FuelTank()
+
+@pytest.fixture
+def car(power_unit, tires, fuel_tank) :
+    return Car(power_unit=power_unit, tires=tires, fuel_tank=fuel_tank)
+
+def test_has_all_attributes(car) :
+    assert hasattr(car, "power_unit")
+    assert hasattr(car, "tires")
+    assert hasattr(car, "fuel_tank")
+    assert hasattr(car, "DRS")
+    assert not car.DRS 
+
+def test_power_unit_error(tires, fuel_tank) :
+    with pytest.raises(TypeError) :
+        Car(1, tires, fuel_tank)
+    with pytest.raises(TypeError) :
+        Car("1", tires, fuel_tank)
+    with pytest.raises(TypeError) :
+        Car([], tires, fuel_tank)
+
+def test_tires_error(power_unit, fuel_tank) :
+    with pytest.raises(TypeError) :
+        Car(power_unit, 1, fuel_tank)
+    with pytest.raises(TypeError) :
+        Car(power_unit, "1", fuel_tank)
+    with pytest.raises(TypeError) :
+        Car(power_unit, [], fuel_tank)
+
+def test_fuel_tank_error(power_unit, tires) :
+    with pytest.raises(TypeError) :
+        Car(power_unit, tires, 1)
+    with pytest.raises(TypeError) :
+        Car(power_unit, tires, "1")
+    with pytest.raises(TypeError) :
+        Car(power_unit, tires, [])
