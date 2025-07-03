2995442efe14954d9a27e66b3b9555b1bbd37a43
Yarno Grenier <127676205+yarno97x@users.noreply.github.com>
2025-07-03 12:34:56 -0400

    Added compounds

--- tests/test_tire.py
+++ tests/test_tire.py
@@ -1,8 +1,37 @@
 import sys
 import os
+import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from tire import *
+
+@pytest.fixture
+def soft() :
+    return Soft()
+
+@pytest.fixture
+def medium() :
+    return Medium()
+
+@pytest.fixture
+def hard() :
+    return Hard()
+
+@pytest.fixture
+def inter() :
+    return Inter()
+
+@pytest.fixture
+def wet() :
+    return Wet()
+
+@pytest.fixture 
+def random_slick() :
+    return random.choice([Soft, Medium, Hard])()
+
+@pytest.fixture
+def random_tire() :
+    return random.choice([Soft, Medium, Hard, Inter, Wet])()
 
 def test_tire_creation() :
     for kind in [Soft, Medium, Hard, Inter, Wet] :
@@ -21,3 +50,51 @@ def test_tire_colors() :
     for kind, hex in colors.items():
         x = kind()
         assert hasattr(x, "color") and x.color == hex
+
+def test_has_temperature(random_tire) :
+    assert hasattr(random_tire, "temperature")
+
+def test_soft_temp_range(soft) :
+    assert 110 >= soft.temperature >= 90
+
+def test_med_temp_range(medium) :
+    assert 120 >= medium.temperature >= 100
+
+def test_hard_temp_range(hard) :
+    assert 130 >= hard.temperature >= 110
+
+def test_inter_temp_range(inter) :
+    assert 80 >= inter.temperature >= 60
+
+def test_wet_temp_range(wet) :
+    assert 70 >= wet.temperature >= 50
+
+def test_has_compound(random_tire) :
+    assert hasattr(random_tire, "compound")
+
+def test_soft_wrong_compound() :
+    with pytest.raises(ValueError) :
+        Soft(compound=Compound.C3)
+    with pytest.raises(ValueError) :
+        Soft(compound=Compound.C2)
+    with pytest.raises(ValueError) :
+        Soft(compound=Compound.C1)
+
+def test_medium_wrong_compound() :
+    with pytest.raises(ValueError) :
+        Medium(compound=Compound.C5)
+    with pytest.raises(ValueError) :
+        Medium(compound=Compound.C4)
+    with pytest.raises(ValueError) :
+        Medium(compound=Compound.C2)
+    with pytest.raises(ValueError) :
+        Medium(compound=Compound.C1)
+
+def test_hard_wrong_compound() :
+    with pytest.raises(ValueError) :
+        Hard(compound=Compound.C5)
+    with pytest.raises(ValueError) :
+        Hard(compound=Compound.C4)
+    with pytest.raises(ValueError) :
+        Hard(compound=Compound.C3)
+
--- tire.py
+++ tire.py
@@ -1,4 +1,7 @@
-from enum import Enum, auto
+from enum import Enum
+import random
+
+RESET = "\033[0m"
 
 class Compound(Enum):
     C1 = "C1"
@@ -6,6 +9,8 @@ class Compound(Enum):
     C3 = "C3"
     C4 = "C4"
     C5 = "C5"
+    I = "I"
+    W = "W"
 
 class TireCategory(Enum):
     HARD = [Compound.C1, Compound.C2]
@@ -13,34 +18,58 @@ class TireCategory(Enum):
     SOFT = [Compound.C4, Compound.C5]
 
 class Tire :
-    def __init__(self):
+    def __init__(self, temperature, compound):
         self.life = 100
+        self.temperature = temperature
+        self.compound = compound
 
 class Slick(Tire) :
-    def __init__(self):
-        super().__init__()
+    def __init__(self, temperature, compound):
+        super().__init__(temperature=temperature, compound=compound)
 
 class Soft(Slick) :
-    def __init__(self):
-        super().__init__()
+    def __init__(self, compound = Compound.C5):
+        if compound not in TireCategory.SOFT.value :
+            raise ValueError("Wrong compound")
+        super().__init__(temperature = random.uniform(90, 110), compound = compound)
         self.color = 0xFF4444
+    
+    def __repr__(self) :
+        return f"{'\033[91m'} [X] {RESET}"
 
 class Medium(Slick) :
-    def __init__(self):
-        super().__init__()
+    def __init__(self, compound = Compound.C3):
+        if compound not in TireCategory.MEDIUM.value :
+            raise ValueError("Wrong compound")
+        super().__init__(temperature = random.uniform(100, 120), compound = compound)
         self.color = 0xFFD700
+    
+    def __repr__(self) :
+        return f"{'\033[93m'} [X] {RESET}"
 
 class Hard(Slick) :
-    def __init__(self):
-        super().__init__()
+    def __init__(self, compound = Compound.C1):
+        if compound not in TireCategory.HARD.value :
+            raise ValueError("Wrong compound")
+        super().__init__(temperature = random.uniform(110, 130), compound = compound)
         self.color = 0xE0E0E0
+    
+    def __repr__(self) :
+        return f"{'\033[97m'} [X] {RESET}"
 
 class Inter(Tire) :
     def __init__(self):
-        super().__init__()
+        super().__init__(temperature = random.uniform(60, 80), compound=Compound.I)
         self.color = 0x33FF33
+
+    def __repr__(self) :
+        return f"{'\033[92m'} [X] {RESET}"
 
 class Wet(Tire) :
     def __init__(self):
-        super().__init__() 
+        super().__init__(temperature = random.uniform(50, 70), compound=Compound.W) 
         self.color = 0x3399FF
+    
+    def __repr__(self) :
+        return f"{'\033[94m'} [X] {RESET}"
+
